af4b014946184b3d4cc44171ad1ad95f3eed5a10
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2022-10-19 09:27:10 +0200

    Add files via upload

---
+++ DateTimePicker/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DateTimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePickerDatum = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerVrijeme = new System.Windows.Forms.DateTimePicker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonRezervacija = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Upišite datum leta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Upišite vrijeme polaska:";
+            // 
+            // dateTimePickerDatum
+            // 
+            this.dateTimePickerDatum.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerDatum.Location = new System.Drawing.Point(168, 60);
+            this.dateTimePickerDatum.Name = "dateTimePickerDatum";
+            this.dateTimePickerDatum.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerDatum.TabIndex = 2;
+            // 
+            // dateTimePickerVrijeme
+            // 
+            this.dateTimePickerVrijeme.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerVrijeme.Location = new System.Drawing.Point(168, 129);
+            this.dateTimePickerVrijeme.Name = "dateTimePickerVrijeme";
+            this.dateTimePickerVrijeme.ShowUpDown = true;
+            this.dateTimePickerVrijeme.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerVrijeme.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(45, 191);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(323, 130);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonRezervacija
+            // 
+            this.buttonRezervacija.Location = new System.Drawing.Point(157, 367);
+            this.buttonRezervacija.Name = "buttonRezervacija";
+            this.buttonRezervacija.Size = new System.Drawing.Size(101, 24);
+            this.buttonRezervacija.TabIndex = 5;
+            this.buttonRezervacija.Text = "Rezervacija";
+            this.buttonRezervacija.UseVisualStyleBackColor = true;
+            this.buttonRezervacija.Click += new System.EventHandler(this.buttonRezervacija_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 403);
+            this.Controls.Add(this.buttonRezervacija);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dateTimePickerVrijeme);
+            this.Controls.Add(this.dateTimePickerDatum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDatum;
+        private System.Windows.Forms.DateTimePicker dateTimePickerVrijeme;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonRezervacija;
+    }
+}
+
